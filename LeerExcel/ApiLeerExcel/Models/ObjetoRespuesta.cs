489506c71f8a34bd8fc232b54208ee363d678b41
Jesús Antero Varela Almanza <52518344+jesusvalm97@users.noreply.github.com>
2022-08-03 12:56:32 -0500

    Try to made an api

---
+++ LeerExcel/ApiLeerExcel/Models/ObjetoRespuesta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiLeerExcel.Models
+{
+    public class ObjetoRespuesta
+    {
+        public ObjetoRespuesta()
+        {
+            Propiedades = new Dictionary<string, object>();
+        }
+
+        /// <summary>
+        /// El key es el nombre de la propiedad y el value el valor de dicha propiedad
+        /// </summary>
+        public Dictionary<string, object> Propiedades { get; }
+    }
+}
